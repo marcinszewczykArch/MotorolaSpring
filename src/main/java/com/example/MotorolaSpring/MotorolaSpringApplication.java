9a9c19b0ade0e22fe870fc6345f69b265fb94ddb
Marcin Szewczyk <marcinszewczyk.arch@gmail.com>
2021-09-12 08:14:23 +0200

    Initial commit

---
+++ src/main/java/com/example/MotorolaSpring/MotorolaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.MotorolaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorolaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorolaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MotorolaSpring/MotorolaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MotorolaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorolaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
